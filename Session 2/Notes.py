05662c0a6a6d5ad7baaedbb94ea565ef19d35382
ChristineKakina <christinekakina@gmail.com>
2025-07-21 10:56:05 +0300

    updates: Calculation of BMI and running the streamlit app

---
+++ Session 2/Notes.py
@@ -0,0 +1,11 @@
+# Using Streamlit to create a web app for Data Science / ML & DL Projects
+# This is the procedure of creating my own virtual env:
+# Creating my virtual environment on python
+#   - change terminal to be cmd and type "python -m venv myDSWDenv"
+#   - Activate the virtual environment ".\myDSWDenv\Scripts\activate"
+#   - (myDSWDenv)C:\Users\ChristineKakina\PycharmProjects\DataScienceWebDevelopment> --------env is activated
+
+#Running the streamlit app navigate to folder and run "streamlit run appName.py":
+# (myDSWDenv) C:\Users\ChristineKakina\PycharmProjects\DataScienceWebDevelopment> cd Session 2
+#
+# (myDSWDenv) C:\Users\ChristineKakina\PycharmProjects\DataScienceWebDevelopment\Session 2>streamlit run app.py
--- Session 2/app.py
+++ Session 2/app.py
@@ -1,1 +1,47 @@
-# Using Streamlit to create a web app for Data Science / ML & DL Projects
+import numpy as np
+import streamlit as st
+from PIL import Image
+from unicodedata import category
+
+#Display the title
+st.title("BMI calculator")
+
+# Load and display the img
+image = Image.open("session2img.jpg")
+st.image(image, use_container_width=True)
+
+#Input fields: Name, height and weight
+name = st.text_input("Enter your name")
+weight = st.number_input("Enter your weight (in kgs)", min_value=0.0, format="%.2f")
+height = st.number_input("Enter your height (in meters)", min_value=0.0, format="%.2f")
+
+
+#Button and logic
+if st.button("Calculate BMI"):
+    if height > 0:
+        # Calculate and categorise the BMI(Weight divided by the square of height)
+        bmi = weight/(height**2)
+        if bmi < 18.5:
+            category= "Underweight"
+        elif 18.5 <= bmi < 24.9:
+            category= "Normal weight"
+        elif 25 <= bmi < 29.9:
+            category= "Overweight"
+        else:
+            category= "Obese"
+
+        st.success(f"{name}, your BMI is {bmi:.2f} which is {category}.")
+
+    else:
+        st.error("Please enter a valid height.")
+
+
+import pandas as pd
+import numpy as np
+
+data = pd.DataFrame({
+    'BMI Range': ["Underweight", "Normal weight", "Overweight", "Obese"],
+    'Min BMI':[0,18,5.25,30],
+    'Max BMI':[18.5,24.9,29.9,40],
+})
+st.bar_chart(data[['Min BMI', 'Max BMI']])
